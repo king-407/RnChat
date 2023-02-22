69eec649b24bf5106deebd4717a41b26161dec9e
king-407 <shivamtiwaritiwari0704@gmail.com>
2023-02-23 03:44:04 +0530

    animations added

--- AfterLogin/Home.js
+++ AfterLogin/Home.js
@@ -1,21 +1,74 @@
-import {View, Text} from 'react-native';
+import {View, Text, ScrollView, TouchableOpacity} from 'react-native';
 import firestore from '@react-native-firebase/firestore';
 import React, {useEffect, useState} from 'react';
-
+import Lottie from 'lottie-react-native';
 const Home = () => {
   const [users, setUsers] = useState([]);
   const getUsers = async () => {
     const querySnap = await firestore().collection('users').get();
-    console.log(querySnap.docs[0].data);
+    const allusers = querySnap._docs.map(docSnap => docSnap.data());
+    setUsers(allusers);
   };
   useEffect(() => {
     getUsers();
   }, []);
-
+  console.log(users);
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <ScrollView>
+      <View style={{flex: 1}}>
+        <Lottie
+          style={{alignSelf: 'center', height: 250, width: 10, marginTop: 20}}
+          source={require('../animations/chatting.json')}
+          autoPlay
+        />
+        <View style={{marginTop: 40}}>
+          {users.map(student => {
+            console.log(student);
+            return (
+              <TouchableOpacity
+                key={student.uid}
+                style={{
+                  marginTop: 10,
+                  height: 150,
+                  width: 350,
+                  backgroundColor: '#FFF2FD',
+                  borderRadius: 35,
+
+                  elevation: 10,
+
+                  alignSelf: 'center',
+                  // flexDirection: 'row',
+                }}>
+                <View>
+                  <Text
+                    style={{
+                      marginLeft: 20,
+                      marginTop: 20,
+                      fontSize: 20,
+                      fontWeight: '450',
+                      fontStyle: 'italic',
+                      color: 'black',
+                    }}>
+                    {student.email}
+                  </Text>
+                  <Text
+                    style={{
+                      marginLeft: 12,
+                      marginTop: 12,
+                      fontSize: 17,
+                      fontStyle: 'italic',
+                      fontWeight: '500',
+                      color: 'black',
+                    }}>
+                    {student.uid}
+                  </Text>
+                </View>
+              </TouchableOpacity>
+            );
+          })}
+        </View>
+      </View>
+    </ScrollView>
   );
 };
 
